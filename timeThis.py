42f4c5fbb3423cbb79506e2ada414ca8c9448bd6
Evgeniy <eugene.v.us@gmail.com>
2018-07-16 22:32:34 +0300

    Add files via upload

---
+++ timeThis.py
@@ -0,0 +1,17 @@
+import time
+from functools import wraps
+
+def timeThis(func):
+    '''
+    Decorator that reports the execution time.
+    '''
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print('\n- = t i m e T h i s = -')
+        print('Function: {}\nargs:{}\nkwargs:{}\nExecution time: {} sec'.format(func.__name__, args, kwargs, end-start))
+        print('- = t i m e T h i s = -\n\n\n')
+        return result
+    return wrapper
