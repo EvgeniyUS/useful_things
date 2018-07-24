2c2dfc8ebe1a8b79a8c00f0237c7fc70ed59ace2
eugene <eugene.v.us@gmail.com>
2018-07-24 11:30:24 +0300

    sorting added

--- timeThis.py
+++ timeThis.py
@@ -4,16 +4,18 @@ from functools import wraps
 
 def timeThis(func):
     '''
-    Decorator that reports the execution time.
+    Decorator that reports the function execution time.
     '''
     @wraps(func)
     def wrapper(*args, **kwargs):
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
-        print('\n- = t i m e T h i s = -')
-        print('Function: {}\nargs:{}\nkwargs:{}\nExecution time: {} sec'.format(
-            func.__name__, args, kwargs, end-start))
-        print('- = t i m e T h i s = -\n\n\n')
+        # print('\n- = t i m e T h i s = -')
+        # print('Function: {}\nargs:{}\nkwargs:{}\nExecution time: {} sec'.format(
+        #     func.__name__, args, kwargs, end-start))
+        print('Function: {}\nExecution time: {} sec'.format(
+            func.__name__, end-start))
+        # print('- = t i m e T h i s = -\n\n\n')
         return result
     return wrapper
