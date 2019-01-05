0c6e1a49b0ea9129be60163432c8eed09822ff30
EvgeniyUs <eugene.v.us@gmail.com>
2019-01-05 14:51:07 +0300

    result to list

--- lsusb_-t_parser.py
+++ lsusb_-t_parser.py
@@ -1,8 +1,9 @@
 from pprint import pprint
 
 memo = dict()
-data = dict()
-for num, line in enumerate(open('lsusb_-t_source.txt', 'r')):
+# data = dict()
+result = list()
+for line in open('lsusb_-t_source.txt', 'r'):
     line = line.replace('\n', '').split(' ')
     try:
         if line[0] == '/:':
@@ -13,27 +14,26 @@ for num, line in enumerate(open('lsusb_-t_source.txt', 'r')):
             memo['previous_indent'] = indent
             memo['previous_device'] = dev_dict
             indent = line.index('|__')
-            dev_dict = dict()
-            dev_dict['device'] = line[indent+1].split('=')[1]
-            dev_dict['class'] = line[indent+2].split('=')[1]
-            dev_dict['bus'] = memo['bus']
-            if dev_dict['class'].lower() == 'hub':
+            dev_dict = {'device': line[indent + 1].split('=')[1],
+                        'class': line[indent + 2].split('=')[1],
+                        'bus': memo['bus']}
+            if 'hub' in dev_dict['class'].lower():
                 dev_dict['input'] = list()
             if not memo['previous_device']:
-                data[memo['bus']] = [dev_dict]
+                # data[memo['bus']] = [dev_dict]
+                result.append(dev_dict)
             else:
                 if indent > memo['previous_indent']:
                     memo[memo['previous_indent']] = memo['previous_device']
                     memo['previous_device']['input'].append(dev_dict)
                 else:
-                    if indent-4 == 0:
-                        data[memo['bus']].append(dev_dict)
+                    if indent - 4 == 0:
+                        # data[memo['bus']].append(dev_dict)
+                        result.append(dev_dict)
                     else:
-                        memo[indent-4]['input'].append(dev_dict)
+                        memo[indent - 4]['input'].append(dev_dict)
     except Exception as e:
-        print num+1
-        print e
+        print(e)
 
-pprint(memo)
-pprint(data)
-
+# pprint(memo)
+pprint(result)
